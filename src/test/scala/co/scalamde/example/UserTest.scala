64fca7a3faa7b178bc5047ffa9f6e36dfeb5c92e
afrancoc2000 <afrancoc2000@gmail.com>
2015-05-06 21:22:34 -0500

    Solución

--- src/test/scala/co/scalamde/example/UserTest.scala
+++ src/test/scala/co/scalamde/example/UserTest.scala
@@ -5,32 +5,36 @@ import org.scalatest.FunSuite
 class UserTest extends FunSuite {
 
   test("Crea un usuario sin genero") {
-    val usuario: Usuario = ???
+    val usuario: Usuario = Usuario(3, "Ana", "Garcia", 31, None)
     assert(usuario.genero === None)
   }
 
   test("Crea un usuario con genero masculino") {
-    val usuario: Usuario = ???
+    val usuario: Usuario = Usuario(3, "Ana", "Franco", 35, Some("masculino"))
     assert(usuario.genero === Some("masculino"))
   }
 
   test("Encuentra la edad del usuario 1") {
-    val edad: Int = ???
+    val usr = UsuarioRepository.findById(1)
+    val edad: Int = usr.map(u => u.edad).getOrElse(0)
     assert(edad === 32)
   }
 
   test("Encuentra el genero del usuario 1") {
-    val genero: Option[String] = ???
+    val usr = UsuarioRepository.findById(1)
+    val genero: Option[String] = usr.flatMap(u => u.genero)
     assert(genero === Some("masculino"))
   }
 
   test("Encuentra el genero del usuario 2"){
-    val genero: Option[String] = ???
+    val usr = UsuarioRepository.findById(2)
+    val genero: Option[String] = usr.flatMap(u => u.genero)
     assert(genero === None)
   }
 
   test("Encuentra el genero del usuario 3"){
-    val genero: Option[String] = ???
+    val usr = UsuarioRepository.findById(2)
+    val genero: Option[String] = usr.flatMap(u => u.genero)
     assert(genero === None)
   }
 }
